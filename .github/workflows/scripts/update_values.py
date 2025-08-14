194da9f112b1ce8d07a591d6856b8d9fbae50e8d
ggorockee <ggorockee@gmail.com>
2025-08-15 01:57:59 +0900

    scrape update

--- .github/workflows/scripts/update_values.py
+++ .github/workflows/scripts/update_values.py
@@ -17,7 +17,7 @@ def update_image_tag(file_path, new_tag):
         with open(file_path) as f:
             data = yaml.load(f)
 
-        data["server"]["image"]["tag"] = new_tag
+        data["scrape"]["image"]["tag"] = new_tag
 
         with open(file_path, "w") as f:
             yaml.dump(data, f)

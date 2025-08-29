7683ce56d3a73354f40d38b7b439f1bcaff5075f
ggorockee <ggorockee@gmail.com>
2025-08-30 00:12:59 +0900

    무한 스크롤 적용

--- scrape/scrapers/mymilky.py
+++ scrape/scrapers/mymilky.py
@@ -119,9 +119,9 @@ class MyMilkyScraper(BaseScraper):
                     lambda driver: len(driver.find_elements(*campaign_list_locator))
                     > item_count_before_scroll
                 )
-                log.info("새로운 아이템 로딩이 확인되었습니다.")
-                if scroll_count == 5:
-                    break
+                # log.info("새로운 아이템 로딩이 확인되었습니다.")
+                # if scroll_count == 5:
+                #     break
 
             except TimeoutException:
                 # 10초간 기다려도 아이템 개수에 변화가 없으면, 페이지 끝으로 판단하고 종료
@@ -272,6 +272,9 @@ class MyMilkyScraper(BaseScraper):
         ).copy()
         dropped_count = len(raw_df) - len(dedup_df)
         if dropped_count > 0:
+            log.info(
+                '중복 제거 ===> ["platform", "title", "offer", "campaign_channel"]'
+            )
             log.info(
                 f"중복된 데이터 {dropped_count}건을 제거했습니다. 처리 대상: {len(dedup_df)}건"
             )

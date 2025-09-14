6343314739561051877535d40f108f25dceee360
ggorockee <ggorockee@gmail.com>
2025-09-14 21:51:32 +0900

    스플래시 로고 그림자 효과 제거
    - splashloading_logo.png 이미지에 이미 그림자가 포함되어 있어 코드에서 그림자 제거
    - BoxDecoration과 ClipRRect 제거로 깔끔한 로고 표시
    - 중복 그림자 효과 방지

--- mobile/lib/screens/splash_screen.dart
+++ mobile/lib/screens/splash_screen.dart
@@ -151,25 +151,11 @@ class _SplashScreenState extends State<SplashScreen>
                       Container(
                         width: logoSize,
                         height: logoSize,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(24.r),
-                          color: Colors.white, // 컨테이너 배경을 흰색으로 설정
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.05), // 그림자 투명도 줄임
-                              blurRadius: 10, // 블러 반경 줄임
-                              offset: const Offset(0, 5), // 그림자 오프셋 줄임
-                            ),
-                          ],
-                        ),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(24.r),
-                          child: Image.asset(
-                            'asset/image/logo/application/reviewmaps.png',
-                            width: logoSize,
-                            height: logoSize,
-                            fit: BoxFit.contain,
-                          ),
+                        child: Image.asset(
+                          'asset/image/logo/application/splashloading_logo.png',
+                          width: logoSize,
+                          height: logoSize,
+                          fit: BoxFit.contain,
                         ),
                       ),
                       

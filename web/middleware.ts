80ff095ba6ea9670874b17f5600bd5b9614f1b04
ggorockee <ggorockee@gmail.com>
2025-11-13 17:25:57 +0900

    Update middleware.ts

--- web/middleware.ts
+++ web/middleware.ts
@@ -9,8 +9,10 @@ export function middleware(request: NextRequest) {
       "google.com, pub-8516861197467665, DIRECT, f08c47fec0942fa0\n",
       {
         headers: {
-          "Content-Type": "text/plain",
-          "Cache-Control": "public, max-age=3600",
+          "Content-Type": "text/plain; charset=utf-8",
+          "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+          "Pragma": "no-cache",
+          "Expires": "0",
         },
       }
     );

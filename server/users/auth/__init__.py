f08ade574c55050acf5c63489487b142ac812dc1
ggorockee <ggorockee@gmail.com>
2025-11-24 23:12:34 +0900

    fix: SNS 로그인 JWT에 email 포함하도록 수정
    문제:
    - SNS 로그인 시 JWT payload에 email: null
    - /auth/me에서 토큰 검증 실패 (401)
    
    원인:
    - api_social.py가 users/auth의 create_access_token(user_id)를 사용
    - email 파라미터가 없는 함수 사용
    
    해결:
    - users/auth/__init__.py에서 users/utils의 토큰 생성 함수 import
    - api_social.py에서 create_access_token(user.id, user.email) 호출
    
    변경 파일:
    - users/auth/__init__.py: utils에서 토큰 함수 import
    - users/api_social.py: kakao/google/apple 로그인에 email 전달

--- server/users/auth/__init__.py
+++ server/users/auth/__init__.py
@@ -1,6 +1,8 @@
 """
 JWT 인증 모듈
 """
-from .jwt_auth import JWTAuth, create_access_token, create_refresh_token
+from .jwt_auth import JWTAuth
+# 토큰 생성 함수는 users.utils에서 가져옴 (email 포함)
+from users.utils import create_access_token, create_refresh_token
 
 __all__ = ['JWTAuth', 'create_access_token', 'create_refresh_token']
